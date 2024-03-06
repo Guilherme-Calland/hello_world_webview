3da6dde532fe5cce88beccfdb7b7d4249b3adca5
Guilherme <gui.crb0@gmail.com>
2024-03-06 15:06:14 -0300

    webview_flutter library

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,159 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(const WebViewPlusExample());
+  runApp(WebViewExample());
 }
 
-class WebViewPlusExample extends StatelessWidget {
-  const WebViewPlusExample({super.key});
+class WebViewExample extends StatelessWidget {
+  WebViewExample({super.key, this.controller});
+
+  WebViewController? controller;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
+    controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..setBackgroundColor(const Color(0x00000000))
+  ..setNavigationDelegate(
+    NavigationDelegate(
+      onProgress: (int progress) {
+        // Update loading bar.
+      },
 
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+      // onPageStarted: (String url) {},
+      onPageFinished: (String url) async{
+        // // Execute JavaScript code to send a message
+        // final result = await controller?.evalJavascript('''
+        //   window.postMessage("Hello from JavaScript");
+        // ''');
 
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
+        // // Handle the result in Flutter
+        // print('Result from JavaScript: $result');
+      },
+      // onWebResourceError: (WebResourceError error) {},
+      // onNavigationRequest: (NavigationRequest request) {
+      //   if (request.url.startsWith('https://www.youtube.com/')) {
+      //     return NavigationDecision.prevent;
+      //   }
+      //   return NavigationDecision.navigate;
+      // },
+    ),
+  )
+  ..addJavaScriptChannel('messageHandler', onMessageReceived: (val){
+    if (kDebugMode) {
+      print(val.message);
+    }
+  })
+  ..loadHtmlString(
 
-class _MainPageState extends State<MainPage> {
-  late WebViewControllerPlus _controler;
+    '''
+      <head>
+          <script>
+              function encryptCreditCard(){
+                  console.log("hello world from test")
+                  // window.postMessage("Hello from JavaScript");
+                  window.messageHandler.postMessage("hello from javascript");
+              }
+          </script>
+          <style>
+              /* Center the content horizontally and vertically */
+              body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh; /* Full height of the viewport */
+                margin: 0; /* Remove default margin */
+                background-color: #e0f2f1;
+              }
+          
+              /* Style for the "HELLO WORLD" message */
+              .hello-world {
+                font-size: 48px; /* Adjust font size as needed */
+                font-weight: bold;
+              }
+            </style>
+      </head>
+      <body onload="encryptCreditCard()">
+          <div class="hello-world">HELLO WORLD</div>
+      </body>
+    '''
 
-  @override
-  void initState() {
-    _controler = WebViewControllerPlus()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(),
-      )
-      ..loadFlutterAssetServer('assets/index.html');
-      
-      
-    super.initState();
-  }
+  )
+  
+  ;
 
-  final double _height = 300;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: 
-      ListView(
-        children: [
-          Text("Height of Webview: $_height",
-              style: const TextStyle(fontWeight: FontWeight.bold)),
-          SizedBox(
-            height: _height,
-            child: WebViewWidget(
-              controller: _controler,
-            ),
-          ),
-          const Text("End of Webview",
-              style: TextStyle(fontWeight: FontWeight.bold)),
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Simple Example')),
+        body: WebViewWidget(controller: controller!),
       ),
     );
   }
-
-  @override
-  void dispose() {
-    _controler.server.close();
-    super.dispose();
-  }
 }
+
+// class WebViewPlusExample extends StatelessWidget {
+//   const WebViewPlusExample({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: MainPage(),
+//     );
+//   }
+// }
+
+// class MainPage extends StatefulWidget {
+//   const MainPage({super.key});
+
+//   @override
+//   State<MainPage> createState() => _MainPageState();
+// }
+
+// class _MainPageState extends State<MainPage> {
+//   late WebViewControllerPlus _controler;
+
+//   @override
+//   void initState() {
+//     _controler = WebViewControllerPlus()
+//       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+//       ..setBackgroundColor(const Color(0x00000000))
+//       ..setNavigationDelegate(
+//         NavigationDelegate(),
+//       )
+//       ..loadFlutterAssetServer('assets/index.html');
+      
+      
+//     super.initState();
+//   }
+
+//   final double _height = 300;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: 
+//       ListView(
+//         children: [
+//           Text("Height of Webview: $_height",
+//               style: const TextStyle(fontWeight: FontWeight.bold)),
+//           SizedBox(
+//             height: _height,
+//             child: WebViewWidget(
+//               controller: _controler,
+//             ),
+//           ),
+//           const Text("End of Webview",
+//               style: TextStyle(fontWeight: FontWeight.bold)),
+//         ],
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _controler.server.close();
+//     super.dispose();
+//   }
+// }
